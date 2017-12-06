aa544b15088d89711434f984ea691a97eb8ea5f4
Jason Cihelka <jason.cihelka@jcace.com>
2017-12-06 16:18:50 -0700

    into weather darksky.net

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,45 @@
 const request = require('request');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
 
-request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=697+hawkwood+blvd+nw+calgary',
-	json: true
-}, (error, response, body) => {
-	console.log(body);
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: 'address',
+// 			descibe: "Address to fetch weather for",
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias('help','h')
+// 	.argv;
+
+// console.log(argv);
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+// 	if (errorMessage) {
+// 		console.log(errorMessage);
+// 	} else {
+// 		console.log(JSON.stringify(results, undefined, 2));
+// 	}
+// });
+
+request( { url: 'https://api.darksky.net/forecast/c369dfc5457aa7a54bc6ca0ad20d2ef1/-51,114',
+	json: true,
+}, 
+(error,response,body) => {
+	if (error) {
+		console.log("Unable to connect to Forecast.io servers");
+	} else if (response.statusCode === 400) 
+	{
+		console.log("Unable to fetch weather.");
+	}
+	else if (response.statusCode === 200) {
+		console.log(body.currently.temperature);
+	}
 });
+
+
+//c369dfc5457aa7a54bc6ca0ad20d2ef1
+
